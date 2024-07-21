74d0623baf5f6b1d873db1186c4bf25112bfc75c
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-21 15:07:04 +0630

    Format Tbl_Blog

--- DotNet8.Hangfire.Api/AppDbContexts/Tbl_Blog.cs
+++ DotNet8.Hangfire.Api/AppDbContexts/Tbl_Blog.cs
@@ -1,15 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿namespace DotNet8.Hangfire.Api.AppDbContexts;
 
-namespace DotNet8.Hangfire.Api.AppDbContexts
+[Table("Tbl_Blog")]
+public class Tbl_Blog
 {
-    [Table("Tbl_Blog")]
-    public class Tbl_Blog
-    {
-        [Key]
-        public int BlogId { get; set; }
-        public string BlogTitle { get; set; }
-        public string BlogAuthor { get; set; }
-        public string BlogContent { get; set; }
-    }
+    [Key]
+    public int BlogId { get; set; }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
 }
