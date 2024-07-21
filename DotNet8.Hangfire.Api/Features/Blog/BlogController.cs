af229575cd3c22ca88bbd268eef5eb6a1e031cad
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-21 15:08:39 +0630

    Format Blog Controller

--- DotNet8.Hangfire.Api/Features/Blog/BlogController.cs
+++ DotNet8.Hangfire.Api/Features/Blog/BlogController.cs
@@ -1,24 +1,20 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿namespace DotNet8.Hangfire.Api.Features.Blog;
 
-namespace DotNet8.Hangfire.Api.Features.Blog
+[Route("api/[controller]")]
+[ApiController]
+public class BlogController : BaseController
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BlogController : BaseController
+    private readonly BL_Blog _bL_Blog;
+
+    public BlogController(BL_Blog bL_Blog)
     {
-        private readonly BL_Blog _bL_Blog;
+        _bL_Blog = bL_Blog;
+    }
 
-        public BlogController(BL_Blog bL_Blog)
-        {
-            _bL_Blog = bL_Blog;
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> GetBlogs()
-        {
-            var lst = await _bL_Blog.GetBlogs();
-            return Content(lst);
-        }
+    [HttpGet]
+    public async Task<IActionResult> GetBlogs()
+    {
+        var lst = await _bL_Blog.GetBlogs();
+        return Content(lst);
     }
 }
--- DotNet8.Hangfire.Api/GlobalUsings.cs
+++ DotNet8.Hangfire.Api/GlobalUsings.cs
@@ -4,4 +4,5 @@ global using System.ComponentModel.DataAnnotations;
 global using System.ComponentModel.DataAnnotations.Schema;
 global using DotNet8.Hangfire.Api.Models;
 global using DotNet8.Hangfire.Api.Repositories.Blog;
+global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.EntityFrameworkCore;
